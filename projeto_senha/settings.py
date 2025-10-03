93ae0b46cd4ddd4f7aef354f7592db1ce5bc917a
Rogério Barbosa <rogeriodosanjosbarbosa2004@gmail.com>
2025-10-03 18:51:51 -0300

    Configurando o projeto e fazendo as migrações

--- projeto_senha/settings.py
+++ projeto_senha/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-zejq$64p^y_2!8(b3m60gob8j+g1)45d=+#508&rf^x!28%2!b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
@@ -102,9 +102,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-BR'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
@@ -115,6 +115,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
