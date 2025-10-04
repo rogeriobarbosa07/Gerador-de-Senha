699d8af63f3bb9b475ba50234ef6df3247e2efaa
Rogério Barbosa <rogeriodosanjosbarbosa2004@gmail.com>
2025-10-03 23:32:52 -0300

    Adicionando view para o site

--- app_senha/views.py
+++ app_senha/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def gerador_senha(request):
+    return render(request, 'app_senha/gerador_senha.html', {}) # renderizando página html
