198604d691ea4def829922c91e7133dbdfcc2e0f
Rogério Barbosa <rogeriodosanjosbarbosa2004@gmail.com>
2025-10-05 16:11:23 -0300

    Adicionando número aleatório de caracteres de cada sequência nas senhas

--- app_senha/funcao_senha.py
+++ app_senha/funcao_senha.py
@@ -1,23 +1,23 @@
 import random
 
-def senha():
+def senha(numero_chars=12):
     chars = ['1234567890', 
              'abcdefghijklmnopqrstuvwxyz', 
              'ABDCEFGHIJKLMNOPQRSTUVWXYZ', 
              '!@#$%&*()-+_=[]\|<>,.:;?/~^']
     
     senha = ''
-    for _ in range(3):
-        senha = senha + random.choice(chars[0])
-        senha = senha + random.choice(chars[1])
-        senha = senha + random.choice(chars[2])
-        senha = senha + random.choice(chars[3])
+    for _ in range(numero_chars):
+        senha = senha + random.choice(chars[random.randint(0, 3)])
 
     senha = list(senha)
     random.shuffle(senha)
     senha = ''.join(senha)
 
     return senha
+
+# testando saída da função
+print(senha())
 
 # def senha_com_valor(valor):
 
